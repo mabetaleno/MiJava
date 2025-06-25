cc4c6044d8872e604ee7f5f84e583daf13170696
mabell taleno <mabell.taleno782@std.uni.edu.ni>
2025-06-25 12:53:41 -0600

    mi primer cambio confimado

---
+++ Holamundo.java
@@ -0,0 +1,15 @@
+// Nombre del archivo: Holamundo.java
+// Descripción: Este programa imprime un saludo y la suma de dos números.
+
+public class Holamundo {
+    public static void main(String[] args) {
+        String nombre = "Estudiante";
+        int a = 5;
+        int b = 3;
+        int suma = a + b;
+
+        System.out.println("Hola, " + nombre + "!");
+        System.out.println("La suma de " + a + " + " + b + " es: " + suma);
+
+    }
+} 
