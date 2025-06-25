74a504029ebd07bdb168b0d25aabe74b7e2c9270
mabell taleno <mabell.taleno782@std.uni.edu.ni>
2025-06-25 13:11:09 -0600

    mi segundo cambio confimado

--- Holamundo.java
+++ Holamundo.java
@@ -4,8 +4,8 @@
 public class Holamundo {
     public static void main(String[] args) {
         String nombre = "Estudiante";
-        int a = 5;
-        int b = 3;
+        int a = 10;
+        int b = 9;
         int suma = a + b;
 
         System.out.println("Hola, " + nombre + "!");
